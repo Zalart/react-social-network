4c50b84d9f26abd1183c5b350b7a97b21249d126
artur <me@zalart.com>
2021-06-09 00:44:16 +0300

    react-router-dom implementation. Routes created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, BrowserRouter} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
 
 
 const App = () => {
   return (<div className="app-wrapper">
+    <BrowserRouter>
    <Header />
    <Navigation />
    <div className="app-wrapper-content">
-   {/* <Profile /> */}
-   <Dialogs />
+     
+     <Route path="/dialogs" component={Dialogs} />
+     <Route path="/profile" component={Profile} />
+     <Route path="/music" component={Music} />
+     <Route path="/news" component={News} />
+     <Route path="/settings" component={Settings} />
+     
    </div>
+   </BrowserRouter>
     </div>
+    
   );
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,15 @@
 import classes from "./Navigation.module.css";
+import {Link} from "react-router-dom";
 
 const Navigation = () => {
     return (
 <nav className={classes.navigation}>
 <ul>
-  <li><a className={classes.item} href="#">Profile</a></li>
-  <li><a className={classes.item} href="#">Messages</a></li>
-  <li><a className={classes.item} href="#">Music</a></li>
-  <li><a className={classes.item} href="#">News</a></li>
-  <li><a className={classes.item} href="#">Settings</a></li>
+  <li><Link className={classes.item} to="/profile">Profile</Link></li>
+  <li><Link className={classes.item} to="/dialogs">Messages</Link></li>
+  <li><Link className={classes.item} to="/music">Music</Link></li>
+  <li><Link className={classes.item} to="/news">News</Link></li>
+  <li><Link className={classes.item} to="/settings">Settings</Link></li>
 
 </ul>
    </nav>

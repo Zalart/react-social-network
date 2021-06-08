ff1d71f53ffb4397e2940cdb2d76ef1f9e305374
artur <me@zalart.com>
2021-06-08 23:43:13 +0300

    Dialog component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 
 const App = () => {
   return (<div className="app-wrapper">
    <Header />
    <Navigation />
-   <Profile />
+   <div className="app-wrapper-content">
+   {/* <Profile /> */}
+   <Dialogs />
+   </div>
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,12 +2,9 @@ import styles from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
-    return (  <div className={styles.content}>
+    return (  <div>
     <div> <img src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg" />
-      </div>
-      <div>
-      <img src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" /></div>
-      
+      </div>      
       <div>ava + description</div>
 <MyPosts />
     </div>
